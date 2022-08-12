8b95ab3fa916e91fedf54f7cc600b1e4531820db
Yunseok Kang <mok02198@gmail.com>
2022-08-12 22:34:07 +0900

    Feat: 스프링 빈 조회

--- Spring-Core/src/main/java/hello/SpringCore/AppConfig.java
+++ Spring-Core/src/main/java/hello/SpringCore/AppConfig.java
@@ -8,21 +8,29 @@ import hello.SpringCore.member.MemberServiceImpl;
 import hello.SpringCore.member.MemoryMemberRepository;
 import hello.SpringCore.order.OrderService;
 import hello.SpringCore.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository()); //생성자 주입
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- Spring-Core/src/main/java/hello/SpringCore/MemberApp.java
+++ Spring-Core/src/main/java/hello/SpringCore/MemberApp.java
@@ -3,13 +3,19 @@ package hello.SpringCore;
 import hello.SpringCore.member.Grade;
 import hello.SpringCore.member.Member;
 import hello.SpringCore.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
         Member findMember = memberService.findMember(1L);
