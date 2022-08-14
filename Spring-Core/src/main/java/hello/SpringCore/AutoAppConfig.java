1d7ba55e652f43f4e440fd3a25a68d55b3bddf93
Yunseok Kang <mok02198@gmail.com>
2022-08-14 18:33:00 +0900

    Test: 필터 기능 테스트

--- Spring-Core/src/main/java/hello/SpringCore/AutoAppConfig.java
+++ Spring-Core/src/main/java/hello/SpringCore/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        basePackages = "hello.SpringCore.member",  //탐색할 패키지의 사작 위치 지정 -> member패키지를 포함해서 하위 패지키만 탐색
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
