d3684b4e66f41640d66f47392154d54e728dbed1
Yunseok Kang <mok02198@gmail.com>
2022-08-17 22:19:29 +0900

    Feat: 롬복 라이브러리 기능 추가

--- Spring-Core/src/main/java/hello/SpringCore/order/OrderServiceImpl.java
+++ Spring-Core/src/main/java/hello/SpringCore/order/OrderServiceImpl.java
@@ -1,36 +1,23 @@
 package hello.SpringCore.order;
 
 import hello.SpringCore.discount.DiscountPolicy;
-import hello.SpringCore.discount.FixDiscountPolicy;
+
 import hello.SpringCore.member.Member;
 import hello.SpringCore.member.MemberRepository;
-import hello.SpringCore.member.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
 //    private final MemberRepository memberRepository = new MemoryMemberRepository(); //DIP 위반
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 //    private final DiscountPolicy discountPolicy = new FixDiscountPolicy(); //DIP 위반(인터페이스에만 의존해야하는 것을 위반) : 인터페이스 뿐 아니라 구체 클래스도 함께 의존하므로
-    private DiscountPolicy discountPolicy;  //DIP 지킴
+    private final DiscountPolicy discountPolicy;  //DIP 지킴
 
-    @Autowired //생성자 1개면 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-    @Autowired
-    public void setMemberRepository(MemberRepository memberRepository){
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired
-    public void setDiscountPolicy(DiscountPolicy discountPolicy){
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
