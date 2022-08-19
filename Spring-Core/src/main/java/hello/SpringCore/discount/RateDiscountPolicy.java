0deab40b7b25dba7bfe71aea952de318fe3f7259
Yunseok Kang <mok02198@gmail.com>
2022-08-19 16:33:33 +0900

    Feat: 조회한 빈이 모두 필요할 때 List,Map 이용

--- Spring-Core/src/main/java/hello/SpringCore/discount/RateDiscountPolicy.java
+++ Spring-Core/src/main/java/hello/SpringCore/discount/RateDiscountPolicy.java
@@ -3,10 +3,11 @@ package hello.SpringCore.discount;
 import hello.SpringCore.annotation.MainDiscountPolicy;
 import hello.SpringCore.member.Grade;
 import hello.SpringCore.member.Member;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@MainDiscountPolicy
+@Primary
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10; //10% 할인
